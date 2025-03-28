84ddb8bd5468f13834e2ce67ead8f0c3d7456aa2
developeragencia <agenciadeveloper01@gmail.com>
2025-03-28 14:40:56 -0300

    fix: atualiza configuracao de variaveis de ambiente do Supabase

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['localhost'],
+  },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.VITE_SUPABASE_DATABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.VITE_SUPABASE_ANON_KEY,
+    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
+    SUPABASE_JWT_SECRET: process.env.SUPABASE_JWT_SECRET,
+  },
+}
+
+module.exports = nextConfig 
